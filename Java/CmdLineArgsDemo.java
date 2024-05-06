c7da56bdb5034bbab99c5fc4a19c24f867145337
Vamshi385 <168412195+Vamshi385@users.noreply.github.com>
2024-05-06 09:58:54 +0530

    Add files via upload

---
+++ Java/CmdLineArgsDemo.java
@@ -0,0 +1,11 @@
+improt java.lang.*;
+public class CmdLineArgsDemo
+{
+public static viod main (string args[])
+{
+System.out.println("command line arguments");
+System .out.println(args[0]);
+System.out.println(args[1]);
+System.out.println(args[2];
+}
+}	
